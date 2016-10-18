ed97c8bb94188fbc8bf30138620799b21229c69b
Pierre Criulanscy <pcriulan@gmail.com>
2016-10-18 15:25:11 +0200

    added gif-viewer-list

--- src/configureStore.js
+++ src/configureStore.js
@@ -3,7 +3,7 @@ import createSagaMiddleware from 'redux-saga'
 import { fork } from 'redux-saga/effects'
 import createLogger from 'redux-logger'
 
-import { root } from './App'
+import root from './root'
 import DevTools from './DevTools'
 import sagaMonitor from './sagaMonitor'
 
